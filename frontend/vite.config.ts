775d3cf7222489f7d0e4f270d24c263bc92ea59d
Eric Nguyen <enguyen807@gmail.com>
2023-11-24 10:18:43 +0200

    Changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: { alias: { '@': '/src' } },
   plugins: [vue()],
+  server: {
+    watch: {
+     usePolling: true,
+    },
+    host: true, // Here
+    strictPort: true,
+    port: 8080, 
+  }
 })
