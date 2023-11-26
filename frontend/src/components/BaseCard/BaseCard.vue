e16ca07cd7912aa15d5ee2a82777136e726c48eb
Eric Nguyen <enguyen807@gmail.com>
2023-11-26 16:04:37 +0200

    Add base card

---
+++ frontend/src/components/BaseCard/BaseCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import "./BaseCard.style.css";
+import { computed } from 'vue';
+
+const props = withDefaults(
+    defineProps<{ 
+        isRounded?: boolean
+        backgroundColor?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'light' | 'dark' }>(), 
+        { isRounded: true, backgroundColor: 'light' }
+);
+
+const classes = computed(() => ({
+  'app-card': true,
+  'app-card--rounded': props.isRounded,
+  [`app-card--${props.backgroundColor || 'light'}`]: true,
+}))
+</script>
+
+<template>
+  <div :class="classes">
+    <slot></slot>
+  </div>
+</template>
