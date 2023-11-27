c1a315845230c42f1e778c2dc979b2bb5d3c5aeb
Eric Nguyen <enguyen807@gmail.com>
2023-11-27 18:29:15 +0200

    Add base input

---
+++ frontend/src/components/BaseInput/BaseInput.vue
@@ -0,0 +1,39 @@
+
+<script setup lang="ts">
+import './BaseInput.style.css';
+
+defineProps<{
+    modelValue: string | number | Date,
+    id: string,
+    label: string,
+    type: string,
+    placeholder?: string,
+    isDisabled?: boolean,
+}>()
+
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: string): void;
+}>();
+
+const handleOnKeyUp = (event: Event): void => {
+    emit("update:modelValue", (event.target as HTMLInputElement).value);
+};
+
+</script>
+
+<template>
+    <div class="app-input">
+        <label :for="id">{{ label }}</label>
+        <input 
+            :id="id" 
+            :type="type" 
+            :placeholder="placeholder" 
+            :value="modelValue" 
+            @input="handleOnKeyUp"
+            min='1900-01-01'
+            :max="new Date().toISOString().split('T')[0]"
+        />
+    </div>
+</template>
+
