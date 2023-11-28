729a1f5acbb17e9d7d690cae03813a4ea53f0337
Eric Nguyen <enguyen807@gmail.com>
2023-11-29 01:08:04 +0200

    Refactor validator

--- backend/src/app/Http/Controllers/CustomerController.php
+++ backend/src/app/Http/Controllers/CustomerController.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
 {
@@ -26,26 +27,28 @@ class CustomerController extends Controller
      */
     public function store(Request $request): JsonResponse
     {
-        $fields = $this->validate($request, [
+        $validate = Validator::make(json_decode($request->getContent(),true), [
             'first_name' => 'required|string|max:50',
             'last_name' => 'required|string|max:50',
             'username' => 'required|string|min:6|max:50|unique:customers,username',
-            'birth_date' => 'required|date_format:d.m.Y|before:today|after:1900-01-01',
+            'birth_date' => 'required|before:today|after:1900-01-01',
             'password' => 'required|min:6|max:50|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/',
         ]);
 
-        $customer = Customer::where('username', '=', $fields['username'])->first();
+        if ($validate->fails()) {
+            if ($validate->errors()->has('username')) {
+                return response()->json(['message'=> 'User already exists!'], JsonResponse::HTTP_CONFLICT);
+            }
 
-        if ($customer) {
-            return response()->json(['message'=> 'User already exists!'], JsonResponse::HTTP_CONFLICT);
+            return response()->json(['error' => $validate->errors()], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
         }
 
         $customer = Customer::create([
-            'first_name' => $fields['first_name'],
-            'last_name'  => $fields['last_name'],
-            'birth_date' => $fields['birth_date'],
-            'username'   => $fields['username'],
-            'password'   => Hash::make($fields['password']),
+            'first_name' => $request->json('first_name'),
+            'last_name'  => $request->json('last_name'),
+            'birth_date' => $request->json('birth_date'),
+            'username'   => $request->json('username'),
+            'password'   => Hash::make($request->json('password')),
         ]);
         return response()->json([ 'data' => $customer ], 200);
     }
@@ -58,7 +61,7 @@ class CustomerController extends Controller
      */
     public function update(int $id, Request $request): JsonResponse
     {
-        $fields = $this->validate($request, [
+        $validate = Validator::make($request->all(), [
             'first_name' => 'required|string|max:50',
             'last_name' => 'required|string|max:50',
             'username' => 'required|string|min:6|max:50|unique:customers,username',
@@ -66,15 +69,19 @@ class CustomerController extends Controller
             'password' => 'required|min:6|max:50|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/',
         ]);
 
+        if ($validate->fails()) {
+            return response()->json(['error' => $validate->errors()], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
         try
         {
             $customer = Customer::findOrFail($id);
             $customer->update([
-                'first_name' => $fields('first_name'),
-                'last_name'  => $fields('last_name'),
-                'birth_date' => $fields('birth_date'),
-                'username'   => $fields('username'),
-                'password'   => Hash::make($fields('password')),
+                'first_name' => $request->json('first_name'),
+                'last_name'  => $request->json('last_name'),
+                'birth_date' => $request->json('birth_date'),
+                'username'   => $request->json('username'),
+                'password'   => Hash::make($request->json('password')),
             ]);
     
             return response()->json([ 'data' => $customer ], 200);
