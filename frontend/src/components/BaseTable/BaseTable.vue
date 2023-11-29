3bb519f35b592f1e77014afade6ff64750083707
Eric Nguyen <enguyen807@gmail.com>
2023-11-29 21:44:09 +0200

    enable multi select

--- frontend/src/components/BaseTable/BaseTable.vue
+++ frontend/src/components/BaseTable/BaseTable.vue
@@ -45,35 +45,33 @@ const classes = computed(() => ({
 </script>
 
 <template>
-
-
-<div class="relative overflow-x-auto">
-    <table :class="classes">
-        <thead>
-            <tr>
-                <th v-if="enableMultiSelect"></th>
-                <th v-for="(header, index) in tableHeaders" :key="index" scope="col">
-                    {{ header }}
-                </th>
-                <th v-if="props.isEditable" scope="col">
-                    Actions
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="row in data" :key="row.id">
-                <td v-if="enableMultiSelect"><input type="checkbox" class="rounded" /></td>
-                <th class="font-medium text-gray-900 whitespace-nowrap">{{ row.id }}</th>
-                <td>{{ row.first_name }}</td>
-                <td>{{ row.last_name }}</td>
-                <td>{{ row.birth_date }}</td>
-                <td>{{ row.username }}</td>
-                <td v-if="props.isEditable">
-                    <BaseButton label="Edit" background-color="warning" @click="onClick(row)"></BaseButton>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+    <div class="relative overflow-x-auto">
+        <table :class="classes">
+            <thead>
+                <tr>
+                    <th v-if="enableMultiSelect"></th>
+                    <th v-for="(header, index) in tableHeaders" :key="index" scope="col">
+                        {{ header }}
+                    </th>
+                    <th v-if="props.isEditable" scope="col">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in data" :key="row.id">
+                    <td v-if="enableMultiSelect"><input type="checkbox" class="rounded" /></td>
+                    <th class="font-medium text-gray-900 whitespace-nowrap">{{ row.id }}</th>
+                    <td>{{ row.first_name }}</td>
+                    <td>{{ row.last_name }}</td>
+                    <td>{{ row.birth_date }}</td>
+                    <td>{{ row.username }}</td>
+                    <td v-if="props.isEditable">
+                        <BaseButton label="Edit" background-color="warning" @click="onClick(row)"></BaseButton>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
